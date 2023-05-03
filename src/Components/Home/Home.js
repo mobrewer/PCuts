0c83de220ed55ad50ea56f06cf76f2e79732bac1
mobrewer <morgiearan@gmail.com>
2023-05-03 17:18:48 -0600

    Added temp image and started the nav bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,23 @@
-export default function Home({}) {
+import React from "react"
+import './Home.css'
+import Temp from '../../Pictures/Temp.png'
+
+
+export default function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Peter Puertas</h1>
+            <div className="temp">
+                <img src={Temp} alt='' />
+            </div>
+            <div className="nav-list">
+                <ul>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="events">Events</a></li>
+                    <li><a href="appointment">Make and Appointement</a></li>
+                    <li><a href="about">About</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
