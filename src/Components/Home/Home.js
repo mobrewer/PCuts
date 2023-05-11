df19cdfb42c0d8b3f0cf125d96726d22c0310dba
mobrewer <morgiearan@gmail.com>
2023-05-10 21:34:47 -0600

    Added some stlying, still working on styling for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,18 +5,18 @@ import Temp from '../../Pictures/Temp.png'
 
 export default function Home() {
     return (
-        <div>
+        <div className="nav">
             <h1>Peter Puertas</h1>
-            <div className="temp">
-                <img src={Temp} alt='' />
-            </div>
             <div className="nav-list">
                 <ul>
-                    <li><a href="home">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="events">Events</a></li>
                     <li><a href="appointment">Make and Appointement</a></li>
                     <li><a href="about">About</a></li>
                 </ul>
+            </div>
+            <div className="temp">
+                <img src={Temp} alt='' />
             </div>
         </div>
     )
