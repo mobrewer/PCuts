e12ba6c31fdce1411acfe4b33dc55a903d5caad3
mobrewer <morgiearan@gmail.com>
2023-05-10 21:49:17 -0600

    Added divs to make styling easier

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,15 +5,26 @@ import Temp from '../../Pictures/Temp.png'
 
 export default function Home() {
     return (
-        <div className="nav">
-            <h1>Peter Puertas</h1>
-            <div className="nav-list">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="events">Events</a></li>
-                    <li><a href="appointment">Make and Appointement</a></li>
-                    <li><a href="about">About</a></li>
-                </ul>
+        <div id="header">
+            <div className="header container">
+                <div className="nav-bar">
+                    <div className="brand">
+                        <a href="/">
+                        <h1><span>P</span>eter <span>P</span>uertas</h1>
+                        </a>
+                    </div>
+                    <div className="nav-list">
+                        <div className="hamburger">
+                            <div className="bar"></div>
+                        </div>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="events">Events</a></li>
+                            <li><a href="appointment">Make and Appointement</a></li>
+                            <li><a href="about">About</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className="temp">
                 <img src={Temp} alt='' />
